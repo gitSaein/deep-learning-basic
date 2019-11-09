d202679022887ae111e613e11ac6ab2ebb88d779
saein.lee <gitSaein@gmail.com>
2019-11-09 12:08:35 +0900

    NAND 수정

--- util/percept.py
+++ util/percept.py
@@ -17,20 +17,21 @@ def AND(param):
         return 1
 def NAND(param):
     if param > 0:
-        return 1
-    else:
         return 0
+    else:
+        return 1
 def OR(param):
     if param <= 0:
         return 0
     else:
         return 1
 
-x = [0,1]
+x = [1,1]
 w = [0.5, 0.5]
 b = -0.7
 or_b = -0.2
 print(AND(BIAS(WEIGHT(x,w), b)))
+print(NAND(BIAS(WEIGHT(x,w),b)))
 print(OR(BIAS(WEIGHT(x,w),or_b)))
 
 
